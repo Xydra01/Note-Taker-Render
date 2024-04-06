a9cef354a956131ec5bb562392ccc4fd188bed10
Xydra <theminecraftgurru@gmail.com>
2024-04-06 19:49:02 -0400

    Please

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // ROUTES
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/api/notes", (req, res) => {
